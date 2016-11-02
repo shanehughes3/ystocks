4bb71b453df037f79821aa3c7ac1ac47003343ba
Shane Hughes <shanehughes314@gmail.com>
2016-11-01 20:07:01 -0500

    module migrated, basic tests

--- test/all.js
+++ test/all.js
@@ -7,8 +7,8 @@ const symbols = ["GOOG", "AAPL"];
 
 Api.quote(symbols, function(err, data, meta) {
     assert.ifError(err);
-    assert.equals(data[0].symbol, "GOOG");
-    assert.equals(data[1].symbol, "AAPL");
+    assert.equal(data[0].symbol, "GOOG");
+    assert.equal(data[1].symbol, "AAPL");
 });
 
 
@@ -20,12 +20,14 @@ const params = {
 
 Api.history(params, function(err, data, meta) {
     assert.ifError(err);
-    assert.equals(data[0].Symbol, "GOOG");
-    assert.equals(data[0].Date, "2016-09-02");
+    assert.equal(data[0].Symbol, "GOOG");
+    assert.equal(data[0].Date, "2016-09-02");
 });
 /*
-Api.history({symbol:'"="', start:"08-30-2016", end:"09-09-2016"},
+Api.history({symbol:'"="', start:"08-30-2016", end:"09-99-2016"},
 	     function(err, data, meta) {
-		 console.log(err, data, meta);
+		 assert.ifError(err);
 	     });
 */
+
+console.log("Pass");
